b1f03056f203a5704f4762377332dd6cd36c3d0a
Janghyun Son <jhson989@gmail.com>
2021-12-30 11:15:54 +0900

    Add functions for printing selected device propeties

---
+++ include/devceiProperties.hpp
@@ -0,0 +1,10 @@
+#ifndef __JH_DEVICE_PROPERTIES__
+#define __JH_DEVICE_PROPERTIES__
+
+#include <CL/sycl.hpp>
+
+void print_properties_cpu();
+void print_properties_gpu();
+void print_properties(sycl::queue& queue);
+
+#endif
---
+++ src/deviceProperties.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdio>
+#include <CL/sycl.hpp>
+namespace sycl = cl::sycl;
+
+void print_properties_cpu() {
+    
+    sycl::host_selector host;
+    sycl::queue host_q(host);
+
+    sycl::device dev = host_q.get_device();
+    std::cout << "=============== Device Properties ==============" << std::endl;
+    std::cout << "Name: " << dev.get_info<sycl::info::device::name>() << std::endl;
+    std::cout << "Vendor: " << dev.get_info<sycl::info::device::vendor>() << std::endl;
+    std::cout << "Memory size: " << dev.get_info<sycl::info::device::global_mem_size>()/1024.0f/1024.0f/1024.0f << " GB"  << std::endl;
+    std::cout << "================================================" << std::endl;
+    
+}
+
+void print_properties_gpu() {
+
+    sycl::gpu_selector gpu;
+    sycl::queue gpu_q(gpu);
+        
+    sycl::device dev = gpu_q.get_device();
+    std::cout << "=============== Device Properties ==============" << std::endl;
+    std::cout << "Name: " << dev.get_info<sycl::info::device::name>() << std::endl;
+    std::cout << "Vendor: " << dev.get_info<sycl::info::device::vendor>() << std::endl;
+    std::cout << "Memory size: " << dev.get_info<sycl::info::device::global_mem_size>()/1024.0f/1024.0f/1024.0f << " GB" << std::endl;
+    std::cout << "================================================" << std::endl;
+
+}
+
+void print_properties(sycl::queue& queue) {
+
+    sycl::device dev = queue.get_device();
+
+    std::cout << "=============== Device Properties ==============" << std::endl;
+    std::cout << "Name: " << dev.get_info<sycl::info::device::name>() << std::endl;
+    std::cout << "Vendor: " << dev.get_info<sycl::info::device::vendor>() << std::endl;
+    std::cout << "Memory size: " << dev.get_info<sycl::info::device::global_mem_size>()/1024.0f/1024.0f/1024.0f << " GB"  << std::endl;
+    std::cout << "================================================" << std::endl;
+}
+
