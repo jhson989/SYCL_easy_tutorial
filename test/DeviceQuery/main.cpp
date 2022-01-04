505750954b212505385bd0712a13eb29ee4c6da3
Janghyun Son <jhson989@gmail.com>
2022-01-04 09:16:00 +0900

    Fix a typo

--- test/DeviceQuery/main.cpp
+++ test/DeviceQuery/main.cpp
@@ -10,6 +10,9 @@ int main(void) {
     // Print host device info
     print_properties_host();
 
+    // Print CPU device info
+    print_properties_cpu();
+
     // Print GPU device info
     print_properties_gpu();
 
